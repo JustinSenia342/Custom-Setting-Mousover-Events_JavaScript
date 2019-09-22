c4f96d5fbe4af35983462f1d24db966716e47e23
Justin Senia <justinsenia342@gmail.com>
2019-09-22 15:22:49 -0400

    FIX FORMATTING

--- js/ProtoFunctions.js
+++ js/ProtoFunctions.js
@@ -1,24 +1,24 @@
-		function randomMove(){
+function randomMove(){
 
-		if (document.getElementById("smallj").checked)
-		{
-			movebox.style.left = 3;
-			movebox.style.left = (parseInt(movebox.style.left) + Math.floor((Math.random() * 10) - 5)) + "px";
-			movebox.style.top = (parseInt(movebox.style.top) + Math.floor((Math.random() * 10) - 5)) + "px";
-		}
-		else if (document.getElementById("mediumj").checked)
-		{
-			movebox.style.left = (parseInt(movebox.style.left) + Math.floor((Math.random() * 60) - 30)) + "px";
-			movebox.style.top = (parseInt(movebox.style.top) + Math.floor((Math.random() * 60) - 30)) + "px";
-		}
-		else if (document.getElementById("bigj").checked)
-		{
-			movebox.style.left = (parseInt(movebox.style.left) + Math.floor((Math.random() * 110) - 55)) + "px";
-			movebox.style.top = (parseInt(movebox.style.top) + Math.floor((Math.random() * 110) - 55)) + "px";
-		}
+if (document.getElementById("smallj").checked)
+{
+	movebox.style.left = 3;
+	movebox.style.left = (parseInt(movebox.style.left) + Math.floor((Math.random() * 10) - 5)) + "px";
+	movebox.style.top = (parseInt(movebox.style.top) + Math.floor((Math.random() * 10) - 5)) + "px";
+}
+else if (document.getElementById("mediumj").checked)
+{
+	movebox.style.left = (parseInt(movebox.style.left) + Math.floor((Math.random() * 60) - 30)) + "px";
+	movebox.style.top = (parseInt(movebox.style.top) + Math.floor((Math.random() * 60) - 30)) + "px";
+}
+else if (document.getElementById("bigj").checked)
+{
+	movebox.style.left = (parseInt(movebox.style.left) + Math.floor((Math.random() * 110) - 55)) + "px";
+	movebox.style.top = (parseInt(movebox.style.top) + Math.floor((Math.random() * 110) - 55)) + "px";
+}
 
-		}
+}
 
-		function Mvr(x){
-		x.style.left = "55px";
-		}
+function Mvr(x){
+x.style.left = "55px";
+}
